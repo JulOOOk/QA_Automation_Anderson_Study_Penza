34b01a4eb110e9292cb3e326451e59e33bd12d0f
Yulia <julok2016@gmail.com>
2018-07-17 00:00:12 +0300

    Добавлена иерархия @Before, @After

--- src/test/java/ru/annina/yulia/YandexTest.java
+++ src/test/java/ru/annina/yulia/YandexTest.java
@@ -5,26 +5,41 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class YandexTest {
 
-    @Test
-    public void search() {
+    WebDriver browser;
+
+    @BeforeSuite
+    public void beforeSuite() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-        WebDriver browser = new ChromeDriver();
+    }
+
+    @BeforeTest
+    public void beforeTest() {
+        browser = new ChromeDriver();
         browser.manage().window().maximize();
         browser.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         browser.get("https://ya.ru/");
+    }
 
+    @Test
+    public void search() {
         WebElement searchInput = browser.findElement(By.name("text"));
         searchInput.clear();
         searchInput.sendKeys("погода пенза");
         WebElement fistSearchText = browser.findElement(By.className("suggest2-item__text"));
         Assert.assertTrue(fistSearchText.getText().contains("пенза"), "О.Р. слово пенза в первой строке");
+    }
 
+    @AfterTest
+    public void afterTest() {
         browser.quit();
     }
 }
