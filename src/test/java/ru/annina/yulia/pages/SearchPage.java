41b90b57c3aa21c60d3dd6e42aab7ff5847a44ab
Yulia <julok2016@gmail.com>
2018-07-23 21:22:01 +0300

    Выполнено ДЗ#4

--- src/test/java/ru/annina/yulia/pages/SearchPage.java
+++ src/test/java/ru/annina/yulia/pages/SearchPage.java
@@ -1,8 +1,11 @@
 package ru.annina.yulia.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class SearchPage {
 
@@ -10,6 +13,10 @@ public class SearchPage {
     private By searchInput = By.name("text");
     private By searchButton = By.className("button_theme_websearch");
     private By firstLink = By.cssSelector("[accesskey='1']");
+    private By geolocationLink = By.className("geolink");
+    private By geolocationInput = By.id("city__front-input");
+    private By tabsMoreLink = By.cssSelector("[data-statlog='tabs.more']");
+    private By tabsMoreList = By.className("home-tabs__more-item");
 
     public SearchPage(WebDriver browser) {
         this.browser = browser;
@@ -26,6 +33,26 @@ public class SearchPage {
     public String getFirstLink() {
         WebElement firstLink = browser.findElement(this.firstLink);
         return firstLink.getText();
+    }
 
+    public void changeGeolocation(String text) {
+        WebElement geolocationLink = browser.findElement(this.geolocationLink);
+        geolocationLink.click();
+        WebElement geolocationInput = browser.findElement(this.geolocationInput);
+        geolocationInput.clear();
+        geolocationInput.sendKeys(text);
+        geolocationInput.submit();
+    }
+
+    public String[] getListMore() {
+        WebElement tabsMoreLink = browser.findElement(this.tabsMoreLink);
+        tabsMoreLink.click();
+        List<WebElement> tabsMoreList = browser.findElements(this.tabsMoreList);
+        String[] titlesMore = new String[tabsMoreList.size()];
+        for (int i = 0; i < tabsMoreList.size(); i++) {
+            WebElement tabsMore = tabsMoreList.get(i);
+            titlesMore[i] = tabsMore.getText();
+        }
+        return titlesMore;
     }
 }
--- src/test/java/ru/annina/yulia/tests/YandexTest.java
+++ src/test/java/ru/annina/yulia/tests/YandexTest.java
@@ -1,25 +1,39 @@
 package ru.annina.yulia.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import ru.annina.yulia.pages.SearchPage;
+
+import java.util.HashMap;
 
 public class YandexTest extends BaseTest {
 
     SearchPage searchPage;
 
-    @BeforeTest
+    @BeforeClass
     @Override
     public void beforeClass() {
         super.beforeClass();
-        browser.get("https://ya.ru/");
         searchPage = new SearchPage(browser);
     }
 
     @Test
     public void search() {
+        browser.get("https://ya.ru/");
         searchPage.search("погода пенза");
         Assert.assertTrue(searchPage.getFirstLink().toLowerCase().contains("погода"), "О.Р. слово погода в первой строке результатов");
+    }
+
+    @Test
+    public void checkGeolocationMore() {
+        browser.get("https://yandex.ru/");
+        searchPage.changeGeolocation("Новороссийск");
+        String[] tabsFirstMore = searchPage.getListMore();
+        searchPage.changeGeolocation("Сызрань");
+        String[] tabsSecondMore = searchPage.getListMore();
+        for (int i = 0; i < tabsFirstMore.length; i++) {
+            Assert.assertEquals(tabsFirstMore[i], tabsSecondMore[i], "О.Р. название вкладок должны совпадать");
+        }
     }
 }
